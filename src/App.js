9ca3a710296f0f8d0db48e15b843ba388d8ff206
tedkkraft <tedkkraft@gmail.com>
2022-03-25 10:35:35 -0400

    Fetch API added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import curtains from './theatre-curtains.png';
 import './App.css';
+import Search from "./components/search"
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={curtains} className="App-logo" alt="logo" />
         <p>
-          Enter your Letterboxd username here
+          Enter your Letterboxd username
         </p>
-            <div class="row">
-                <div class="form-group col text-center">
-                    <input type="username" class="form-control text-center" id="songTitle" placeholder="Song"/>
-                </div>
-            </div>
 
-            <div class="row justify-content-center cbod">
-                <button type="submit" class="btn btn-danger w-25 m-3 btn-search" id="sub">Search</button>
+          <div class="row">
+            <div class="form-group col text-center">
+              <input type="username" class="form-control text-center" id="letterboxdUsername" placeholder="enter username"/>
             </div>
+          </div>
 
+          <div class="row justify-content-center cbod">
+            <button type="submit" class="btn btn-danger w-25 m-3 btn-search" id="sub">Search</button>
+          </div>
       </header>
     </div>
   );
